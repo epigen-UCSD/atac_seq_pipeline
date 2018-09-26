2ade8298d0072caa5fc3e592b3865ef7e7d1a58f
frank@tscc <zhangc518@gmail.com>
2018-09-26 15:12:15 -0700

    fixed the bug in screen on trim

--- utils/runFastQC_screen.sh
+++ utils/runFastQC_screen.sh
@@ -4,6 +4,7 @@
 sample_prefix=$1
 FASTQDIR="/projects/ps-epigen/seqdata/"
 OUTPUT_DIR="/projects/ps-epigen/outputs/libQCs/${sample_prefix}"
+WORKDIR="/oasis/tscc/scratch/$(whoami)/outputs/"
 
 mkdir -p $OUTPUT_DIR
 
@@ -21,7 +22,8 @@ do
 
     echo "running fastscreen $p ..."
     pre="${sample_prefix}_R${i}.trim"
-    [[ ! -f "${FASTQDIR}${pre}.fastq.gz" ]] &&  pre=${pre/.trim/}
+    pf=$(find ${WORKDIR}$sample_prefix -name "${pre}.fastq.gz")
+    [[ -z $pf ]] &&  pre=${pre/.trim/}
     pf="${FASTQDIR}${pre}.fastq.gz"
     tagged_fastq="$OUTPUT_DIR/${pre}.tagged.fastq.gz"
     fastq_screen --threads 16  --outdir $OUTPUT_DIR --force --tag --subset 100000 $pf 
