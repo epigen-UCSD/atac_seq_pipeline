76f405b9d9f9abe9eaf5a413d738dc7b48847ba0
frank@tscc <zhangc518@gmail.com>
2019-04-08 15:50:45 -0700

    add script to recover all snapcnt

---
+++ utils/recover_snap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+FASTQDIR="/projects/ps-epigen/seqdata/"
+export PATH=$PATH:/projects/ps-epigen/software/SNAP-CHIP_epicypher
+
+## find snap log files
+cd ~/data/outputs/libQCs/
+
+find . -name "*snap*.log" | while read l
+do
+    pre=${l/.snap.log/};
+    if [ ! -f ${pre}.snap.cnt.tab ]
+    then
+        fq=${FASTQDIR}/${pre##*/}
+        echo ${pre}.snap.cnt.tab
+        echo ${pre%/*}
+        echo $fq
+        bash epicypher.sh -i $fq -m 2 -k true -o ${pre%/*} 1 | tee ${pre}.snap.cnt.tab
+    fi
+done
+
+## log all changes 
+# find . -name "*snap*.cnt.tab" -mmin -30  > ~/data/logs/app/2019-04-08_snap_recover.txt
